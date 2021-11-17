80d13b0e3aa87ce336c5e8a3e15ee0231dbfb787
Grim's Desktop <paulperson.pro@gmail.com>
2021-11-17 09:55:15 +0100

    Init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
+
 module.exports = {
-  reactStrictMode: true,
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  trailingSlash: true,
+  // Your NextJS config.
+};
+  
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { PlasmicRootProvider } from "@plasmicapp/react-web"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }: AppProps) {
+  return (
+    <PlasmicRootProvider>
+      <Component {...pageProps} />
+    </PlasmicRootProvider>
+  )
 }
 export default MyApp
